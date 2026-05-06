3da434bc6f0099c046f416e56a14ffb4eecf0a17
quang13092003 <ngocquangho95@gmail.com>
2026-05-07 02:23:07 +0700

    Add store method to KhachHangController

--- backend_bida/app/Http/Controllers/KhachHangController.php
+++ backend_bida/app/Http/Controllers/KhachHangController.php
@@ -25,4 +25,24 @@ class KhachHangController extends Controller
             'data' => $khachHang
         ]);
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ten_khach_hang' => 'required|string|max:255',
+            'so_dien_thoai' => 'required|string|max:15|unique:khach_hangs',
+            'hang_thanh_vien' => 'in:Thường,VIP'
+        ]);
+
+        $kh = KhachHang::create([
+            'ten_khach_hang' => $request->ten_khach_hang,
+            'so_dien_thoai' => $request->so_dien_thoai,
+            'hang_thanh_vien' => $request->hang_thanh_vien ?? 'Thường'
+        ]);
+
+        return response()->json([
+            'status' => 1,
+            'message' => 'Thêm khách hàng thành công.',
+            'data' => $kh
+        ]);
+    }
 }
