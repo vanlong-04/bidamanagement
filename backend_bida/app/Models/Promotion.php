981dfb63565945210bf965d5843087a56832407f
quang13092003 <ngocquangho95@gmail.com>
2026-05-07 02:14:38 +0700

    Add basic Promotion model structure

---
+++ backend_bida/app/Models/Promotion.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promotion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'promotions';
+    protected $primaryKey = 'promotion_id';
+    public $timestamps = true;
+
+}
