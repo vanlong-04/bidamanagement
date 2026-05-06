2f3862f5011d3fb6f0e5fb0e7dc0cdbd632b7570
quang13092003 <ngocquangho95@gmail.com>
2026-05-07 02:15:40 +0700

    Add basic HoaDonController class

---
+++ backend_bida/app/Http/Controllers/HoaDonController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\HoaDon;
+use App\Models\Ban;
+use App\Models\ChiTietHoaDon;
+use App\Models\DichVu;
+use App\Models\Promotion;
+use Carbon\Carbon;
+
+class HoaDonController extends Controller
+{
+}
