7e60bdf9cf2502d39f4421f5241542376ac816c0
trsonna <trantrungson10b3@gmail.com>
2026-05-07 08:46:48 +0700

    feat(fe): cap nhat router de dang ky component quan ly ban cho Son

--- FE_BIDA/src/router/index.js
+++ FE_BIDA/src/router/index.js
@@ -40,6 +40,11 @@ const routes = [
         component: () => import('../components/Test/index.vue')
       },
       {
+        path: 'dashboard',
+        component: () => import('../components/Test/index.vue')
+      },
+      // Routes của Sơn - Quản lý Bàn
+      {
         path: 'ban',
         component: () => import('../components/admin/ban/index.vue')
       },
