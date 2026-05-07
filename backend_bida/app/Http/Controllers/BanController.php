2e136d8096044588c0977077734afba9a4391e2f
trsonna <trantrungson10b3@gmail.com>
2026-05-07 08:39:15 +0700

    feat(controller): khoi tao BanController voi namespace va method index

---
+++ backend_bida/app/Http/Controllers/BanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ban;
+
+class BanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+}
